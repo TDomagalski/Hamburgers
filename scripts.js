8f344eab24fa55bd78cdb43c8341a20340e06320
Tomasz <tomekwebdev@gmail.com>
2020-04-02 16:59:33 +0200

    first animation done, global styles done and it works !!

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const items = document.querySelectorAll('.item');
+
+for (let i = 0; i < items.length; i++) {
+  items[i].addEventListener('click', function () {
+    this.classList.toggle('active');
+    console.log(this);
+  }, false);
+}
